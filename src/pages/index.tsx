78eb13f55a656c68a3a8b56a29d5752f5fb823e1
pikapika2 <yossii0108@gmail.com>
2022-09-22 17:31:18 +0900

    add qr code reader
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react'
+import { QrReader } from 'react-qr-reader'
+
 export default function Home() {
+  const [data, setData] = useState('No result')
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text)
+          }
+
+          if (!!error) {
+            console.info(error)
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
   )
 }

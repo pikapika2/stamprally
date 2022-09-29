57fe8525bb47fe8b64cb67d5b5e1e6d2bf7fd92d
pikapika2 <yossii0108@gmail.com>
2022-09-29 15:53:21 +0900

    delete style.css
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import '../style/style.css'
+//import '../style/style.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

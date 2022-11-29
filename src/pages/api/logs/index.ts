4fb0335932b3cf8f73bc93ed9a350a4530b6a362
pikapika2 <yossii0108@gmail.com>
2022-11-29 14:23:11 +0900

    add definition req and res
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/api/logs/index.ts
+++ src/pages/api/logs/index.ts
@@ -1,4 +1,9 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   console.log(req.body)
   res.status(200).json({ name: 'John Doe' })
 }
